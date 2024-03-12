4c37df758f0dc6b56b911b6e82105f1b6478e3f5
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-12 13:32:35 +0100

    Added example, but no bin files when installing.

--- add-example.bat
+++ add-example.bat
@@ -1,9 +1,8 @@
 @echo off
 IF [%1] == [] GOTO :end
 
+git submodule add -b %1 git@github.com:ciscoheat/superforms-examples-base.git examples\%1
 pushd examples\%1
-git submodule add -b %1 git@github.com:ciscoheat/superforms-examples-base.git %1
-cd %1
 pnpm install
 popd
 
--- remove-example.bat
+++ remove-example.bat
@@ -4,5 +4,6 @@ IF [%1] == [] GOTO :end
 git rm examples\%1
 rm -rf .git\modules\examples\%1
 git config --remove-section submodule.examples/%1
+echo Remember to commit.
 
 :end
