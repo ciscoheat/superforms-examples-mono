2a64de0748a942fd9c8675830a573caf946677fa
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-12 13:12:21 +0100

    Trying again

--- add-example.bat
+++ add-example.bat
@@ -2,9 +2,8 @@
 IF [%1] == [] GOTO :end
 
 pushd examples\%1
-git submodule add https://github.com/ciscoheat/superforms-examples-base.git %1
+git submodule add -b %1 git@github.com:ciscoheat/superforms-examples-base.git %1
 cd %1
-git remote set-url --push origin https://nopush.example.com
 pnpm install
 popd
 
